4e523d9fae52e6a3c8d76b59e80ef286e8912c61
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-06-02 22:10:46 +0200

    add: menu in main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from data_ripper.modules import PswChecker
+import colorama
 
 def main():
+    
+    print(f""" 
+        [{Fore.BLUE}1{Style.RESET_ALL}] Password Checker
+    """)
+
+    choice = input(f"{Fore.GREEN}[?] Select your choice => {Style.RESET_ALL}")
+
