cea124fc07329912470f5a48d99943a128f8fff0
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-06-02 22:17:12 +0200

    add: if statement in main.py

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def main():
 
     choice = input(f"{Fore.GREEN}[?] Select your choice => {Style.RESET_ALL}")
 
+    if choice == "1":
+        psw_to_check = input(f"{Fore.GREEN}[?] Insert password to check => {Style.RESET_ALL}")
+        checker = PswChecker(psw_to_check)
+        checker.check_password()
+    else:
+        print(f"{Fore.GREEN}[X] Invalid Choice{Style.RESET_ALL}")
