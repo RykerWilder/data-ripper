c5103f5a0ec6c207725879c6e883d3529b725074
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-06-04 12:19:46 +0200

    fix: __init__.py

--- data_ripper/__init__.py
+++ data_ripper/__init__.py
@@ -1,3 +1,3 @@
-from data_ripper.utils import print_welcome_message
+from data_ripper.utils import print_welcome_message, exit
 
-__all__ = ["print_welcome_message"]
+__all__ = ["print_welcome_message", "exit"]
