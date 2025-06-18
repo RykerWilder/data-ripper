1bbaa5089ef18a880c8d989560329e1584a250fc
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-06-18 11:06:12 +0200

    add: api request on shodan

--- data_ripper/modules/devices_vulnerabilites.py
+++ data_ripper/modules/devices_vulnerabilites.py
@@ -5,5 +5,14 @@ import shodan
 class DevicesVulnerabilities():
 
     api_key = os.getenv("SHODAN_API_KEY")
-
     api = shodan.Shodan(api_key)
+
+    def search_shodan(query):
+    try:
+        results = api.search(query)
+        for result in results['matches']:
+            print(f"IP: {result['ip_str']}")
+            print(f"Port: {result['port']}")
+            print(f"Data: {result['data'][:100]}...\n")
+    except shodan.APIError as e:
+        print(f"Error: {e}")
