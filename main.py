7cc8a600325df21380011eb319c7767a42d593a7
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-06-19 09:39:31 +0200

    add: while loop in menu

--- main.py
+++ main.py
@@ -10,26 +10,27 @@ def main():
 
     print_welcome_message()
     
-    print(f""" 
-        [{Fore.BLUE}1{Style.RESET_ALL}] Password Checker
-        [{Fore.BLUE}2{Style.RESET_ALL}] Email Checker
-        [{Fore.BLUE}3{Style.RESET_ALL}] Get info about domain
-    """)
+    while True:
+        print(f""" 
+            [{Fore.BLUE}1{Style.RESET_ALL}] Password Checker
+            [{Fore.BLUE}2{Style.RESET_ALL}] Email Checker
+            [{Fore.BLUE}3{Style.RESET_ALL}] Get info about domain
+        """)
 
-    choice = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Select your choice => ")
+        choice = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Select your choice => ")
 
-    if choice == "1":
-        psw_to_check = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Insert password to check => ")
-        psw_checker = PswChecker(psw_to_check)
-        psw_checker.check_password()
-    elif choice == "2":
-        email_to_check = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Insert email to check => ")
-        EmailChecker().email_checker_manager(email_to_check)
-    elif choice == "3":
-        domain_to_check = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Insert domain to check => ")
-        GetDomainInfo().domain_info_manager(domain_to_check)
-    else:
-        print(f"{Fore.RED}[X] Invalid Choice{Style.RESET_ALL}")
+        if choice == "1":
+            psw_to_check = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Insert password to check => ")
+            psw_checker = PswChecker(psw_to_check)
+            psw_checker.check_password()
+        elif choice == "2":
+            email_to_check = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Insert email to check => ")
+            EmailChecker().email_checker_manager(email_to_check)
+        elif choice == "3":
+            domain_to_check = input(f"{Fore.GREEN}[?]{Style.RESET_ALL} Insert domain to check => ")
+            GetDomainInfo().domain_info_manager(domain_to_check)
+        else:
+            print(f"{Fore.RED}[X] Invalid Choice{Style.RESET_ALL}")
 
 
 if __name__ == "__main__":
