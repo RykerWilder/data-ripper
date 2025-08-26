77720792106be62e3aa5b1b6dcd11f54c8075b5e
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-08-26 12:04:38 +0200

    fix: mac_address_info name

--- data_ripper/modules/__init__.py
+++ data_ripper/modules/__init__.py
@@ -3,6 +3,6 @@ from data_ripper.modules.email_checker import EmailChecker
 from data_ripper.modules.get_domain_info import GetDomainInfo
 from data_ripper.modules.json_converter import JSONConverter
 from data_ripper.modules.username_checker import UsernameChecker
-from data_ripper.modules.macaddress_info import MACLookup
+from data_ripper.modules.mac_address_info import MACLookup
 
 __all__ = ["PswChecker", "EmailChecker", "GetDomainInfo", "JSONConverter", "UsernameChecker", "MACLookup"]
